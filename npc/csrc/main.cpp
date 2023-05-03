e19bbce8751f8d1bf5625b8ed9efb179e9e5625a
jixiang <wysjixiang@163.com>
2023-05-03 18:40:11 +0800

    Adding verification for sub and blt module

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,13 +4,10 @@
 #include "Vtop.h"				// contains the top class of alu module
 #include <nvboard.h>
 
-#define MAX_SIM_TIME 30
-
-
+#define MAX_SIM_TIME 3000
 
 static TOP_NAME dut;
 void nvboard_bind_all_pins(Vtop *top);
-
 
 int main(){
 		Vtop *dut_sim = new Vtop;
@@ -18,15 +15,44 @@ int main(){
     nvboard_init();
     Verilated::traceEverOn(true);
 		VerilatedVcdC *m_trace = new VerilatedVcdC;
-		dut_sim -> trace(m_trace,5);
+		dut_sim -> trace(m_trace,2);
 		m_trace -> open("waveform.vcd");
 
+
+		// sub verification
 		uint64_t sim_time = 0;
-		while(sim_time < MAX_SIM_TIME){
-			dut_sim -> sw = rand()%4;
-			dut_sim->eval();
-			m_trace -> dump(sim_time);
-			sim_time++;
+		bool flag = 1;
+		int16_t a =0;
+		int16_t b = 0;
+		uint8_t op = 1;
+		while(flag){
+			op = 1;
+			//dut_sim -> sw = rand()%4;
+			for(a = -8; a<8; a++){
+				for(b=-8;b<8;b++){
+					dut_sim->sw = ((a<<4 | (b & 0xF)) &0xFF ) | (op << 13);
+					dut_sim->eval();
+					m_trace -> dump(sim_time);
+					sim_time++;
+					if(a == 7 && b == 7) flag = 0;
+				}
+			}
+		}
+
+		// blt verification
+		flag = 1;
+		op = 7;
+		while(flag){
+			//dut_sim -> sw = rand()%4;
+			for(a = -8; a<8; a++){
+				for(b=-8;b<8;b++){
+					dut_sim->sw = ((a<<4 | (b & 0xF)) &0xFF ) | (op << 13);
+					dut_sim->eval();
+					m_trace -> dump(sim_time);
+					sim_time++;
+					if(a == 7 && b == 7) flag = 0;
+				}
+			}
 		}
 		m_trace -> close();
 		delete dut_sim;
