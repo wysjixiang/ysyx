3ebd5f0b2c7d00c9495f68334a5c267b08a879db
jixiang <wysjixiang@163.com>
2023-05-29 13:35:19 +0800

    Implement DiffTest!

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -18,7 +18,13 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for(int i=0;i<32;i++){
+    if(ref_r->gpr[i] != cpu.gpr[i]){
+      printf("Difftest Failed! PC@%lx\n",pc);
+      return false;
+    }
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
