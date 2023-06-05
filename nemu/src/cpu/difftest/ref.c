55514b28a9105d920d0d24d817e8141ad1ba44f0
jixiang <wysjixiang@163.com>
2023-06-06 04:42:46 +0800

    add codes to ref.c

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -40,3 +40,17 @@ __EXPORT void difftest_init(int port) {
   /* Perform ISA dependent initialization. */
   init_isa();
 }
+
+
+/*
+// 在DUT host memory的`buf`和REF guest memory的`addr`之间拷贝`n`字节,
+// `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction);
+// `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+// `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
+void difftest_regcpy(void *dut, bool direction);
+// 让REF执行`n`条指令
+void difftest_exec(uint64_t n);
+// 初始化REF的DiffTest功能
+void difftest_init();
+*/
